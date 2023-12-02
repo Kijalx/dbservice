388d68a3d4ad952504e30b8dd26e597b0cdbe2c3
aleks <alekskijewski@gmail.com>
2023-12-02 14:02:24 +0000

    fixed mongo

--- microservices/routes/index.js
+++ microservices/routes/index.js
@@ -1,52 +1,56 @@
-let express = require('express');
-let router = express.Router();
+// routes/index.js
 
-let Mongoose = require('mongoose').Mongoose;
-let Schema = require('mongoose').Schema;
-let oldMong = new Mongoose();
+const express = require('express');
+const router = express.Router();
+const Mongoose = require('mongoose').Mongoose;
+const Schema = require('mongoose').Schema;
+const oldMong = new Mongoose();
 oldMong.connect('mongodb://mongodb:27017/db', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let meetingSchema = new Schema({
-  meetingId: String,
+const bookSchema = new Schema({
+  bookId: String,
   title: String,
   image: String,
-  address: String,
+  author: String,
   description: String
-}, { collection: 'meetings' });
+}, { collection: 'books' });
 
-let meetings = oldMong.model('meetings', meetingSchema);
+const books = oldMong.model('books', bookSchema);
 
 router.get('/', async function (req, res, next) {
-  const meetings = await getMeetings();
-  res.render('index');
+  const booksData = await getBooks();
+  res.render('index', { books: booksData.books });
 });
 
-router.post('/getMeetings', async function (req, res, next) {
-  const meetings = await getMeetings();
-  res.json(meetings);
+router.post('/getBooks', async function (req, res, next) {
+  const booksData = await getBooks();
+  res.json(booksData);
 });
 
-async function getMeetings() {
-  data = await meetings.find().lean();
-  return { meetings: data };
+async function getBooks() {
+  try {
+    const data = await books.find().lean();
+    return { books: data };
+  } catch (err) {
+    console.error('Error fetching books:', err);
+    return { books: [], error: 'Failed to fetch books' };
+  }
 }
 
-router.post('/saveMeeting', async function (req, res, next) {
-  const meetings = await saveMeeting(req.body);
-  res.json(meetings);
+router.post('/saveBook', async function (req, res, next) {
+  const saveResult = await saveBook(req.body);
+  res.json(saveResult);
 });
 
-async function saveMeeting(theMeeting) {
-  console.log('theMeeting: ' + theMeeting);
-  await meetings.create(theMeeting,
-    function (err, res) {
-      if (err) {
-        console.log('Could not insert new meeting')
-        return { saveMeetingResponse: "fail" };
-      }
-    }
-  )
-  return { saveMeetingResponse: "success" };
+async function saveBook(theBook) {
+  try {
+    console.log('theBook: ', theBook);
+    await books.create(theBook);
+    return { saveBookResponse: "success" };
+  } catch (err) {
+    console.error('Could not insert new book:', err);
+    return { saveBookResponse: "fail", error: 'Failed to save book' };
+  }
 }
 
 module.exports = router;
